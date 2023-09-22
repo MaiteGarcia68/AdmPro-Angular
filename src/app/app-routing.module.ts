0beb479f94d1abf4ea9ad176b017edbf135662dd
Maite Garcia <mt.garcia.arr@gmail.com>
2023-09-22 16:09:44 -0300

    Rutas creadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,35 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { Page404Component } from './pages/page404/page404.component';
-import { PagesComponent } from './pages/pages.component';
+import { PageRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+import { Page404Component } from './page404/page404.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    ]
-  },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
 
+  // path: '/dashborad' PageRouting
+  // path: '/auth' AuthRouting
+
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', component: Page404Component },
 ]
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot( routes )
+    RouterModule.forRoot( routes ),
+    PageRoutingModule,
+    AuthRoutingModule,
   ],
   exports: [ RouterModule ]
 })
